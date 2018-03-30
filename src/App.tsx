35e039c5eb2476eb8ebfa660d9b16868df664460
Ludwig Frank <lufra2606@gmail.com>
2018-03-30 19:23:22 +0200

    added datamaps example test

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import * as React from 'react';
-import './App.css';
+import * as React from 'react'
+import Map from './components/Map'
+import './App.css'
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">BAM Visualization</h1>
-        </header>
-        <p className="App-intro">
-          Hello
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Map />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
