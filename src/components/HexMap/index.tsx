014027579a0f9712b6dacaf7644033105e79537c
Ludwig Frank <lufra2606@gmail.com>
2018-04-18 11:55:04 +0200

    * code cleanup

--- src/components/HexMap/index.tsx
+++ src/components/HexMap/index.tsx
@@ -9,9 +9,9 @@ import { Hexbin, hexbin as d3Hexbin } from 'd3-hexbin'
 // import { polygonContains as d3PolygonContains } from 'd3-polygon'
 import usJson from '../../data/us-states.json'
 import * as topojson from 'topojson-client'
-import { MultiPolygon, Polygon, Position } from 'geojson'
+import { MultiPolygon, Polygon } from 'geojson'
 import { DataPoint } from '../../types'
-import { calculatePointGrid, getPointsInPolygon, renderHexagons } from './util'
+import { calculatePointGrid, getPointsInPolygon, renderHexagons, getParsedFeature } from './util'
 
 import Map from './Map'
 
@@ -29,6 +29,7 @@ interface Props {
 interface State {
     featureCollection: ExtendedFeatureCollection<ExtendedFeature<GeoGeometryObjects, any>>
     projection: GeoProjection
+    pointGrid: Array<DataPoint>
 }
 
 const width = window.innerWidth
@@ -42,8 +43,6 @@ const hexBin: Hexbin<DataPoint> = d3Hexbin<DataPoint>()
     .x(function(d: any) { return d.x })
     .y(function(d: any) { return d.y })
 
-const pointGrid = calculatePointGrid(cols, width, height)
-
 interface FeatureProperties {
     name: string
 }
@@ -56,7 +55,8 @@ export default class HexMap extends React.Component <Props, State> {
 
         this.state = {
             featureCollection: usJson,
-            projection: geoAlbers().scale(800).translate([(width - 75) / 2, height / 2])
+            projection: geoAlbers().scale(800).translate([(width - 75) / 2, height / 2]),
+            pointGrid: calculatePointGrid(cols, width, height)
         }
     }
 
@@ -68,39 +68,16 @@ export default class HexMap extends React.Component <Props, State> {
         return topojson.feature(data, data.objects.usa)
     }
 
-    renderPolygonPoints = (points: Array<DataPoint>, polygon: Array<[number, number]>, data: any): void => {
-        const pointsInPolygon = getPointsInPolygon(points, polygon, data)
-        renderHexagons(pointsInPolygon, this.canvas, hexBin, data)
-    }
-
     renderFeature = (feature: ExtendedFeature<any, FeatureProperties>, geoProjection: GeoProjection) => {
+        const { projection, pointGrid } = this.state
         const name = feature.properties.name
-        const geometryCoordinates = feature.geometry.coordinates
-        const geometryType = feature.geometry.type
-        let polygon: Array<[number, number]> = []
 
-        const render = (coordinates: Position[]): void => {
-            coordinates.map((coordinate: Position) => {
-                polygon.push(geoProjection([coordinate[0], coordinate[1]]) as [number, number])
-            })
-            this.renderPolygonPoints(pointGrid, polygon, {name})
-            polygon = []
+        const logPoly = (polygon: any) => {
+            const pointsInPolygon = getPointsInPolygon(pointGrid, polygon, {name})
+            renderHexagons(pointsInPolygon, this.canvas, hexBin, {name})
         }
 
-        if (geometryType === 'Polygon') {
-            geometryCoordinates.map((coordinates: Position[]) => {
-                render(coordinates)
-            })
-        } else if (geometryType === 'MultiPolygon') {
-            geometryCoordinates.map((coordinateGroup: Position[][]) => {
-                coordinateGroup.map((coordinates: Position[]) => {
-                    render(coordinates)
-                })
-            })
-        } else {
-            throw new Error(`The specified feature type is not valid: ${feature.geometry.type}.
-                    Make sure the data contains only Polygon or MultiPolygon geometry.`)
-        }
+        getParsedFeature(feature, projection, logPoly)
     }
 
     drawGeo = async (data: ExtendedFeatureCollection<ExtendedFeature<Polygon | MultiPolygon, FeatureProperties>>) => {
--- src/components/HexMap/util.tsx
+++ src/components/HexMap/util.tsx
@@ -3,6 +3,8 @@ import { select as d3Select, selectAll as d3SelectAll } from 'd3-selection'
 import { polygonContains as d3PolygonContains } from 'd3-polygon'
 import { DataPoint } from '../../types'
 import { Hexbin } from 'd3-hexbin'
+import { MultiPolygon, Polygon, Position } from 'geojson'
+import { ExtendedFeature, GeoProjection } from 'd3-geo'
 
 export const calculatePointGrid = (cols: number, width: number, height: number): Array<DataPoint> => {
     const hexDistance = width / cols
@@ -68,4 +70,51 @@ export const getPointsInPolygon = (
         }
     })
     return pointsInPolygon
+}
+
+/**
+ * Returns the projected coordinates of the given coordinate.
+ */
+export const getProjection = (coordinate: Position, geoProjection: GeoProjection): Position => {
+    return geoProjection([coordinate[0], coordinate[1]]) as [number, number]
+}
+
+interface FeatureProperties {
+    name: string
+}
+
+/**
+ * Parses a feature to generate:
+ * @param feature: the GeoFeature consisting of Polygons or MultiPolygon as geometry
+ * @param geoProjection: GeoProjection
+ * @param geometryCallback: function that gets called with each individual polygon.
+ */
+export const getParsedFeature = (
+    feature: ExtendedFeature<any, FeatureProperties>, geoProjection: GeoProjection, geometryCallback: Function):
+    ExtendedFeature<Polygon | MultiPolygon, FeatureProperties> => {
+    const geometryCoordinates = feature.geometry.coordinates
+    const geometryType = feature.geometry.type
+
+    if (geometryType === 'Polygon') {
+        const polygon = geometryCoordinates.map((coordinates: Position[]) => {
+            return coordinates.map((coordinate: Position) => getProjection(coordinate, geoProjection))
+        })
+        geometryCallback(polygon[0])
+        return {...feature, geometry: {...feature.geometry, projectedCoordinates: polygon}}
+
+    } else if (geometryType === 'MultiPolygon') {
+        const multiPolygon: Position[][][] = []
+        geometryCoordinates.map((multiCoordinates: Position[][]) => {
+            const polygon = multiCoordinates.map((coordinates: Position[]) => {
+                return coordinates.map((coordinate: Position) => getProjection(coordinate, geoProjection))
+            })
+            geometryCallback(polygon[0])
+            multiPolygon.push(polygon)
+        })
+        return {...feature, geometry: {...feature.geometry, projectedCoordinates: multiPolygon}}
+    } else {
+        throw new Error(`The specified feature type is not valid: ${feature.geometry.type}.
+                    Make sure the data contains only Polygon or MultiPolygon geometry.`)
+    }
+
 }
