7be277147b9543447dc9660c160f7161fcf45bb6
Florian Zia <florianzia@flozia.fritz.box>
2018-04-26 22:32:38 +0200

    feat: Apply styling to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,18 +17,21 @@ const SidebarContainer = styled.div`
 `;
 const BackButton = styled.div`
     border: 2px solid red;
-    padding-top: 10px;
+    line-height: 34px;
+    padding-left: 45px;
     position: relative;
+    text-align: left;
 
     &::before {
         background-color: white;
-        border: 2px solid gray;
+        // border: 2px solid gray;
         border-radius: 100%;
         box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
         content: '<';
         height: 30px;
         left: 0;
         line-height: 28px;
+        text-align: center;
         top: 0;
         position: absolute;
         width: 30px;
@@ -51,9 +54,10 @@ const AddDataButton = styled.button`
     color: white;
     font-size: 16px;
     left: 50%;
+    padding: 10px 0;
     position: absolute;
     transform: translate(-50%, 0);
-    width: 150px;
+    width: 90%;
 `;
 
     export default class Sidebar extends React.Component {
@@ -89,71 +93,93 @@ const AddDataButton = styled.button`
                 
                 <SidebarContent>
                     {'I want to see'}
-                    <span>
-                        <Select
-                            name="form-field-name"
-                            value={selectedOption}
-                            onChange={this.handleSelectChange}
-                            options={[
-                                { value: 'dentalMedicine', label: 'dental medicine' },
-                                { value: 'physician', label: 'physician' },
-                            ]}
-                        />
-                        {'students at'}
-                        <Select
-                            name="form-field-name"
-                            value={selectedOption}
-                            onChange={this.handleSelectChange}
-                            options={[
-                                { value: 'allInstitutions', label: 'all institutions' },
-                                { value: 'havard', label: 'havard' },
-                            ]}
-                        />
-                        {'who came from'}
-                        <Select
-                            name="form-field-name"
-                            value={selectedOption}
-                            onChange={this.handleSelectChange}
-                            options={[
-                                { value: 'virginia ', label: 'Virginia ' },
-                                { value: 'newYork', label: 'New York' },
-                            ]}
-                        />
-                        {'between'}
-                        <Select
-                            name="form-field-name"
-                            value={selectedOption}
-                            onChange={this.handleSelectChange}
-                            options={[
-                                { value: '1860', label: '1860' },
-                                { value: '1861', label: '1861' },
-                            ]}
-                        />
-                        {'and'}
-                        <Select
-                            name="form-field-name"
-                            value={selectedOption}
-                            onChange={this.handleSelectChange}
-                            options={[
-                                { value: '1980', label: '1980' },
-                                { value: '1981', label: '1981' },
-                            ]}
-                        />
-                        {'.'}
-                        {'I want to see'}
-                        {'who are'}
-                        <Select
-                            name="form-field-name"
-                            value={selectedOption}
-                            onChange={this.handleSelectChange}
-                            options={[
-                                { value: 'female', label: 'female' },
-                                { value: 'male', label: 'male' },
-                                { value: 'femaleMale', label: 'female male' }
-                            ]}
-                        />
-                        {'.'}
-                    </span>
+                    <Select
+                        name="form-field-name"
+                        value={selectedOption}
+                        wrapperStyle={{
+                            display: 'inline-block',
+                            minWidth: '110px'
+                        }}
+                        onChange={this.handleSelectChange}
+                        options={[
+                            { value: 'dentalMedicine', label: 'dental medicine' },
+                            { value: 'physician', label: 'physician' },
+                        ]}
+                    />
+                    {'students at'}
+                    <Select
+                        name="form-field-name"
+                        value={selectedOption}
+                        wrapperStyle={{
+                            display: 'inline-block',
+                            minWidth: '110px'
+                        }}
+                        onChange={this.handleSelectChange}
+                        options={[
+                            { value: 'allInstitutions', label: 'all institutions' },
+                            { value: 'havard', label: 'havard' },
+                        ]}
+                    />
+                    {'who came from'}
+                    <Select
+                        name="form-field-name"
+                        value={selectedOption}
+                        wrapperStyle={{
+                            display: 'inline-block',
+                            minWidth: '110px'
+                        }}
+                        onChange={this.handleSelectChange}
+                        options={[
+                            { value: 'virginia ', label: 'Virginia ' },
+                            { value: 'newYork', label: 'New York' },
+                        ]}
+                    />
+                    {'between'}
+                    <Select
+                        name="form-field-name"
+                        value={selectedOption}
+                        wrapperStyle={{
+                            display: 'inline-block',
+                            minWidth: '110px'
+                        }}
+                        onChange={this.handleSelectChange}
+                        options={[
+                            { value: '1860', label: '1860' },
+                            { value: '1861', label: '1861' },
+                        ]}
+                    />
+                    {'and'}
+                    <Select
+                        name="form-field-name"
+                        value={selectedOption}
+                        wrapperStyle={{
+                            display: 'inline-block',
+                            minWidth: '110px'
+                        }}
+                        onChange={this.handleSelectChange}
+                        options={[
+                            { value: '1980', label: '1980' },
+                            { value: '1981', label: '1981' },
+                        ]}
+                    />
+                    {'.'}
+                    {'I want to see'}
+                    {'who are'}
+                    <Select
+                        name="form-field-name"
+                        value={selectedOption}
+                        wrapperStyle={{
+                            display: 'inline-block',
+                            minWidth: '110px'
+                        }}
+                        onChange={this.handleSelectChange}
+                        options={[
+                            { value: 'female', label: 'female' },
+                            { value: 'male', label: 'male' },
+                            { value: 'femaleMale', label: 'female male' }
+                        ]}
+                    />
+                    {'.'}
 
                 </SidebarContent>
 
