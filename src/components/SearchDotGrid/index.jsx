62fd53d2931e1b4f8f2d3ce5f2f5b37bdfc2c420
Florian Zia <florianzia@flozia.fritz.box>
2018-04-27 18:25:05 +0200

    particle container test

--- src/components/SearchDotGrid/index.jsx
+++ src/components/SearchDotGrid/index.jsx
@@ -28,21 +28,76 @@ export default class SearchDotGrid extends React.Component {
     }
     componentDidMount() {
         // Initilize pixi canvas
-        this.app = new PIXI.Application(window.innerWidth, window.innerHeight);
+        // this.app = new PIXI.Application(window.innerWidth, window.innerHeight);
+        // this.pixiCanvas.appendChild(this.app.view);
+        // this.app.start();
+        
+        // this.dotSprite = PIXI.Sprite.fromImage(dot);
+        // this.dotSprite.x = this.app.screen.width / 2;
+        // this.dotSprite.y = this.app.screen.height / 2;
+        // this.app.stage.addChild(this.dotSprite);
+
+        // animate sprites
+        // this.app.ticker.add((delta) => {
+        //     // just for fun, let's rotate mr rabbit a little
+        //     // delta is 1 if running at 100% performance
+        //     // creates frame-independent transformation
+        //     this.dotSprite.rotation += 0.1 * delta;
+        // });
+
+        this.app = new PIXI.Application();
         this.pixiCanvas.appendChild(this.app.view);
         this.app.start();
         
-        this.dotSprite = PIXI.Sprite.fromImage(dot);
-        this.dotSprite.x = this.app.screen.width / 2;
-        this.dotSprite.y = this.app.screen.height / 2;
-        this.app.stage.addChild(this.dotSprite);
-
-        this.app.ticker.add((delta) => {
-            // just for fun, let's rotate mr rabbit a little
-            // delta is 1 if running at 100% performance
-            // creates frame-independent transformation
-            this.dotSprite.rotation += 0.1 * delta;
+        var sprites = new PIXI.particles.ParticleContainer(10000, {
+            scale: true,
+            position: true,
+            rotation: true,
+            uvs: true,
+            alpha: true
         });
+        this.app.stage.addChild(sprites);
+        
+        // create an array to store all the sprites
+        var maggots = [];
+        
+        var totalSprites = this.app.renderer instanceof PIXI.WebGLRenderer ? 10000 : 100;
+        
+        for (var i = 0; i < totalSprites; i++) {
+        
+            // create a new Sprite
+            var dude = PIXI.Sprite.fromImage(dot);
+        
+            dude.tint = Math.random() * 0xE8D4CD;
+        
+            // set the anchor point so the texture is centerd on the sprite
+            dude.anchor.set(0.5);
+        
+            // different maggots, different sizes
+            dude.scale.set(0.8 + Math.random() * 0.3);
+        
+            // scatter them all
+            dude.x = Math.random() * this.app.screen.width;
+            dude.y = Math.random() * this.app.screen.height;
+        
+            dude.tint = Math.random() * 0x808080;
+        
+            // create a random direction in radians
+            dude.direction = Math.random() * Math.PI * 2;
+        
+            // this number will be used to modify the direction of the sprite over time
+            dude.turningSpeed = Math.random() - 0.8;
+        
+            // create a random speed between 0 - 2, and these maggots are slooww
+            dude.speed = (2 + Math.random() * 2) * 0.2;
+        
+            dude.offset = Math.random() * 100;
+        
+            // finally we push the dude into the maggots array so it it can be easily accessed later
+            maggots.push(dude);
+        
+            sprites.addChild(dude);
+        }
 
     }
     componentWillUnmount() {
