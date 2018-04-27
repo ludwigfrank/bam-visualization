da894311a05c53917ea4da9e00bbab49c20b85dd
Florian Zia <florianzia@flozia.fritz.box>
2018-04-27 22:22:07 +0200

    Add text search input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,33 @@ import HelpButton from '../components/HelpButton';
 
 const SearchBarContainer = styled.div`
     border: 2px solid blue;
+    height: 100px;
+    position: relative;
     width: 100%;
+
+    input[type=text] {
+        border: none;
+        border-bottom: 2px solid gray;
+        font-size: 18px;
+        padding: 5px;
+        width: 50vw;
+    }
+    input[type=submit] {
+        background-color: orange;
+        border: none;
+        border-radius: 4px;
+        color: white;
+        font-size: 16px;
+        padding: 10px 30px;
+        margin-left: 30px;
+    }
+`;
+const FormContainer = styled.div`
+    display: inline-table;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
 `;
 
 export default class SearchBar extends React.Component {
@@ -17,8 +43,19 @@ export default class SearchBar extends React.Component {
     render () {
         return (
             <SearchBarContainer>
-                {'search bar container'}
                 <HelpButton />
+                <FormContainer>
+                    <form>
+                        <label>
+                            <input
+                                type={'text'}
+                                name={'search'}
+                                placeholder={'Enter physician’s name, school, place or search for document content'}
+                            />
+                            <input type={'submit'} value={'Seach Archive'} />
+                        </label>
+                    </form>
+                </FormContainer>
             </SearchBarContainer>
         );
     }
